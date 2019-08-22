99a369e447017bfd5e2139b549a04bc28206ebe7
April.Chen <forchpeng@163.com>
2019-08-22 19:37:49 +0800

    Initial commit

---
+++ src/main/java/com/chanpion/cloud/eureka/EurekaAdminApplication.java
@@ -0,0 +1,13 @@
+package com.chanpion.cloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaAdminApplication.class, args);
+	}
+
+}
