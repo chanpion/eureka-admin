2a2d8d35813a6be78a5681c89635c37d4996261b
April.Chen <forchpeng@163.com>
2019-08-27 21:22:55 +0800

    服务管理

--- src/main/java/com/chanpion/cloud/eureka/controller/ServiceController.java
+++ src/main/java/com/chanpion/cloud/eureka/controller/ServiceController.java
@@ -10,7 +10,6 @@ import com.netflix.eureka.EurekaServerContextHolder;
 import com.netflix.eureka.registry.PeerAwareInstanceRegistry;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.*;
--- src/main/resources/templates/common/header.ftl
+++ src/main/resources/templates/common/header.ftl
@@ -2,7 +2,7 @@
     <!-- Logo -->
     <a href="" class="logo">
         <!-- mini logo for sidebar mini 50x50 pixels -->
-        <span class="logo-mini"><b>E</b>ureka</span>
+        <span class="logo-mini"><b>Eu</b></span>
         <!-- logo for regular state and mobile devices -->
         <span class="logo-lg"><b>Eureka</b></span>
     </a>
